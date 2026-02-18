bd0ee3e397dd58b74c7ac7c566dfc02f3216769b
ArsalanJdev <arsalanjdev@gmail.com>
2026-02-18 18:19:15 +0330

    Implement fastapi inference endpoint

--- src/fastapi/app.py
+++ src/fastapi/app.py
@@ -6,24 +6,48 @@ import torch
 import json
 import io
 
-app = FastAPI(title="FastImageCaptioning", summary="Generate a caption based on the image you upload.", version="0.1.0")
+app = FastAPI(
+    title="FastImageCaptioning",
+    summary="Generate a caption based on the image you upload.",
+    version="0.1.0",
+)
 
-def get_model(vocab_size: int,model_weights: str = "model_weights.pt"):
-    model = HybridModelAttention(vocab_size)
-    state_dict = torch.load(model_weights)
-    model.load_state_dict(state_dict)
-    return model
 
-@app.post("/upload"):
+def load_vocab(vocab_json_path: str):
+    with open(vocab_json_path, "r") as f:
+        data = json.load(f)
+
+    word2idx = data.get("word2idx", {})
+    idx2word_raw = data.get("idx2word", {})
+    idx2word = {int(k): v for k, v in idx2word_raw.items()}
+
+    return {"word2idx": word2idx, "idx2word": idx2word}
+
+
+# 1. Device
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
+
+# 2. Load vocab once
+vocab = load_vocab("vocab.json")
+vocab_size = len(vocab["word2idx"])
+
+# 3. Load model once
+model = HybridModelAttention(vocab_size).to(device)
+state_dict = torch.load("model_weights.pt", map_location=device)
+model.load_state_dict(state_dict)
+model.eval()  # important!
+
+
+@app.post("/upload")
 async def upload_image(file: UploadFile = File()):
     try:
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        with open("vocab.json") as f:
-            vocab = json.load(f)
-        model = get_model(len(vocab), model_weights="model_weights.pt")
+        # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        device = torch.device("cpu")
+        vocab = load_vocab("vocab.json")
         contents = await file.read()
         image = Image.open(io.BytesIO(contents))
         caption = generate_caption(image=image, device=device, vocab=vocab, model=model)
         return {"caption": caption}
     except Exception as e:
-        return HTTPException(400,str(e))
+        return HTTPException(400, str(e))
